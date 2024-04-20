82cbe0cfdbece4d392f2a0c702deb8251758a85a
Ivanch024 <ivanorozco2404@gmail.com>
2024-04-20 12:02:42 -0500

    recuperación de contraseña pro

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from './Components/LoginForm/LoginForm';
 import Register from './Components/Register/Register';
+import RecoverPassword from "./Components/RecoverPassword/RecoverPassword";
 
 
 
@@ -9,9 +10,9 @@ const App = () => {
     return (
         <Router>
             <Routes>
-               <Route path="/Login"  element={<LoginForm />} /> {/** ruta de login */}
+                <Route path="/Login"  element={<LoginForm />} /> {/** ruta de login */}
                 <Route path="/Register" element={<Register />} /> {/** ruta de inicio de la app */}
-               
+                <Route path="/RecoverPassword" element={<RecoverPassword />} /> {/** ruta de recuperación de contraseña */}
             </Routes>
         </Router>
     )
